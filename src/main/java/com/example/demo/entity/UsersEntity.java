0dc50af4264c4a0dcca738a2194c5bc9273a7f95
Felipe <felipe.pcollantes@gmail.com>
2020-04-16 11:06:03 +0200

    Generate UsersEntity

---
+++ src/main/java/com/example/demo/entity/UsersEntity.java
@@ -0,0 +1,63 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "users")
+@Entity(name = "Users")
+public class UsersEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+	private String apellidos;
+	private String email;
+	private String fechaNacimiento;
+	private Long edad;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public Long getEdad() {
+		return edad;
+	}
+	public void setEdad(Long edad) {
+		this.edad = edad;
+	}
+	
+	
+
+}
