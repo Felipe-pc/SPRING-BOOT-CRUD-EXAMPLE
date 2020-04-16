8a6921e2dd1b9195ce40b1871b51518b1e2d8f50
Felipe <felipe.pcollantes@gmail.com>
2020-04-16 11:25:52 +0200

    Create UserServiceBddImpl

---
+++ src/main/java/com/example/demo/service/impl/UserServiceBddImpl.java
@@ -0,0 +1,55 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.UsersEntity;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.IUserService;
+
+import es.soltel.rest.entity.PersonaEntity;
+import es.soltel.rest.repository.PersonaRepository;
+
+@Service("UserServiceBdd")
+public class UserServiceBddImpl implements IUserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public List<UsersEntity> findAll() {
+		return userRepository.findAll();
+	}
+
+	@Override
+	public UsersEntity findById(Long id) {
+		Optional<UsersEntity> user = userRepository.findById(id);
+		return user.isPresent() ? user.get() : null;
+	}
+
+	@Override
+	public UsersEntity add(UsersEntity user) {
+		return userRepository.save(user);
+	}
+
+	@Override
+	public UsersEntity update(UsersEntity user) {
+		Optional<UsersEntity> userBdd = userRepository.findById(user.getId());
+
+		if (userBdd.isPresent()) {
+			userBdd.get().setNombre(user.getNombre());
+			return userRepository.save(userBdd.get());
+		}
+
+		return null;
+	}
+
+	@Override
+	public void remove(Long id) {
+		userRepository.deleteById(id);
+	}
+
+}
